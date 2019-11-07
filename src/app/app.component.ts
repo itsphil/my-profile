80cf0e94842730f54273139c6975910a7fcf6994
itsphilz <philzeelte@gmail.com>
2019-11-07 15:52:04 +0000

    added new font, and styling to profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'My Profile',
       url: '/home',
-      icon: 'home'
+      icon: 'person'
     }
   ];
 
