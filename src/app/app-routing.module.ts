4255cb02d84a9a6de89da4a4a73757ae627f98fe
itsphilz <philzeelte@gmail.com>
2019-11-07 16:59:16 +0000

    moved header into it's own component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'profile',
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.HomePageModule)
   }
 ];
 
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -4,7 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { ComponentsModule } from '../../components/components.module';
+
+import { ProfilePage } from './profile.page';
 
 @NgModule({
   imports: [
@@ -14,10 +16,11 @@ import { HomePage } from './home.page';
     RouterModule.forChild([
       {
         path: '',
-        component: HomePage
+        component: ProfilePage
       }
-    ])
+    ]),
+    ComponentsModule
   ],
-  declarations: [HomePage]
+  declarations: [ProfilePage]
 })
 export class HomePageModule {}
